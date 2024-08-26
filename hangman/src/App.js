3a5d89c7b510efbf431633ed8e3176af6b3976da
Venya <venyadurgam25@gmail.com>
2024-08-25 20:12:01 -0500

    Updating wrong letters Function

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -3,8 +3,6 @@ import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
-import Popup from './components/Popup';
-import Notification from './components/Notification';
 
 import './App.css';
 
@@ -25,7 +23,7 @@ function App() {
       <Header/>
       <div className = "game-container">
         <Figure/>
-        <WrongLetters/>
+        <WrongLetters wrongLetters = {wrongLetters}/>
         <Word selectedWord = {selectedWord} correctLetters = {correctLetters}/>
       </div>
     </>
